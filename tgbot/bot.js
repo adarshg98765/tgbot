c3c6de42c1ca4676128123d32481ae3a91dd01a9
ADARSH G <adarshg98765@gmail.com>
2021-06-16 18:57:23 +0530

    done

--- tgbot/bot.js
+++ tgbot/bot.js
@@ -8,7 +8,9 @@ function state(ctx){
 	const tomorrow = new Date(today)
 	tomorrow.setDate(tomorrow.getDate() + 1)
 	var d = tomorrow.getDate();
+	var d1 = today.getDate();
 	const sturl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=300&date=${d}-06-2021`;
+	const sturl1 = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=300&date=${d1}-06-2021`;
 	fetch(sturl , {
       credentials: 'include',
       method: 'GET',
@@ -16,15 +18,32 @@ function state(ctx){
   	.then(response=>response.json())
   	.then(data=>{
   		var len = Object.keys(data['sessions']).length;
-  		for(i=0;i<len-2;i++){
+  		for(i=0;i<len-1;i++){
   			if(data['sessions'][i]['available_capacity_dose1']>0 && data['sessions'][i]['min_age_limit']<40){
-  				bot.telegram.sendMessage(ctx.chat.id,'available');
+  				bot.telegram.sendMessage(ctx.chat.id,'available tomorrow');
 				  break;
   			}
   		}
   	})
 	.catch(err=>{});
-	  setTimeout(function(){ state(ctx) }, 100000);
+
+	fetch(sturl1 , {
+      credentials: 'include',
+      method: 'GET',
+      headers: {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}})
+  	.then(response=>response.json())
+  	.then(data=>{
+  		var len = Object.keys(data['sessions']).length;
+  		for(i=0;i<len-1;i++){
+  			if(data['sessions'][i]['available_capacity_dose1']>0 && data['sessions'][i]['min_age_limit']<40){
+  				bot.telegram.sendMessage(ctx.chat.id,'available today');
+				  break;
+  			}
+  		}
+  	})
+	.catch(err=>{});
+
+	setTimeout(function(){ state(ctx) }, 100000);
 };
 bot.start((ctx) => {
 	ctx.reply("started");
