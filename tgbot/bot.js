371a86e468e4a413d6b5aa6adfc7418ca12c8f71
ADARSH G <adarshg98765@gmail.com>
2021-06-17 20:28:27 +0530

    hi

--- tgbot/bot.js
+++ tgbot/bot.js
@@ -38,7 +38,7 @@ function state(ctx,id){
 		.then(data=>{
 			var len = Object.keys(data['sessions']).length;
 			for(i=0;i<len-1;i++){
-				if(data['sessions'][i]['available_capacity_dose1']>0 && data['sessions'][i]['min_age_limit']==18){
+				if(data['sessions'][i]['available_capacity_dose1']==0 && data['sessions'][i]['min_age_limit']==18){
 					bot.telegram.sendMessage(ctx.chat.id,'Vaccine available today');
 					bot.telegram.sendMessage(ctx.chat.id,'If you want to continue the search, type /start');
 					return;
